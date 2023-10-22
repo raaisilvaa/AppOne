e47b5e784551af16286d1dd48264aa4126383430
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-10-21 22:13:53 -0300

    atualização

--- app/src/main/java/com/raaisilvaa/appone/MainActivity.kt
+++ app/src/main/java/com/raaisilvaa/appone/MainActivity.kt
@@ -2,33 +2,38 @@ package com.raaisilvaa.appone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.TextView
-import org.w3c.dom.Text
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textOne = findViewById<TextView>(R.id.textOne)
-        val textTwo = findViewById<TextView>(R.id.textTwo)
-        val buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
-        val buttonClear = findViewById<Button>(R.id.buttonClear)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val etEmail = findViewById<TextView>(R.id.etEmail)
+        val btSend = findViewById<Button>(R.id.btSend)
+        val btClear = findViewById<Button>(R.id.btClear)
 
-        // Button Send Behavior
-        buttonSubmit.setOnClickListener {
-            val email = textTwo.text.toString()
+        btSend.setOnClickListener {
+            val email = etEmail.text.toString()
 
+
+            // is blank verifica se o campo foi digitado corretamente
             if(email.isBlank()) {
-                textTwo.error = "E-mail inválido!"
+                etEmail.error = "Digite seu e-mail"
+            // Patterns verifica se o campo foi digitado com o @ ou faltandl algo
+            } else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                etEmail.error = "E-mail inválido!"
             } else {
-                textOne.text = textTwo.text.toString()
+                tvResult.text = etEmail.text.toString()
             }
-        }
-        buttonClear.setOnClickListener {
-            textOne.text = ""
-            textTwo.text = ""
+            // método de fazer o botão limpar efetuar o disparo de limpar
+        btClear.setOnClickListener {
+            tvResult.text = ""
+            etEmail.text = null
         }
     }
 }
+}
+
