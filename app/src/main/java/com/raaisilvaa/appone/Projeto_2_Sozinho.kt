b1ecc64c2228ab9a0e2c35467c385decd1a17d6b
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-10-22 18:58:53 -0300

    Atualizando o aplicativo com a opção de receber um error apenas no campo que estiver realmente incorreto, pois antes mesmo um campo sendo verdadeiro ele mostrava o error desse campo.

--- app/src/main/java/com/raaisilvaa/appone/Projeto_2_Sozinho.kt
+++ app/src/main/java/com/raaisilvaa/appone/Projeto_2_Sozinho.kt
@@ -17,29 +17,54 @@ class Projeto_2_Sozinho : AppCompatActivity() {
         val btEnviar = findViewById<TextView>(R.id.btEnviar)
         val btLimpar = findViewById<TextView>(R.id.btLimpar)
 
+        // Aqui, você está declarando duas variáveis booleanas, emailError e phoneError,
+        // para rastrear se há erros nos campos de e-mail e telefone.
+        // Inicialmente, eles são definidos como false, o que significa que não há erros.
+        var emailError = false
+        var phoneError = false
+
         btEnviar.setOnClickListener {
             val email = textEmail.text.toString()
             val phone = numPhone.text.toString()
 
+            // Você está limpando quaisquer erros anteriores, definindo as mensagens de erro para null,
+            // para garantir que as mensagens de erro anteriores não sejam exibidas.
+            textEmail.error = null
+            numPhone.error = null
 
-            if(email.isBlank() || phone.isBlank()) {
-                textEmail.error = "Digite seu e-mail"
-                numPhone.error = "Digite seu telefone"
-
-            } else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            if (email.isBlank() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 textEmail.error = "E-mail inválido!"
-                numPhone.error = "Número de telefone inválido!"
+                emailError = true
             } else {
-                textResult.text = textEmail.text.toString()
-                textResultPhone.text = numPhone.text.toString()
+                emailError = false
             }
 
-            btLimpar.setOnClickListener {
-                textResultPhone.text = ""
-                textResult.text = ""
-                textEmail.text = null
-                numPhone.text = null
+            if (phone.isBlank()) {
+                numPhone.error = "Digite seu telefone"
+                phoneError = true
+            } else {
+                phoneError = false
             }
+            // Aqui, você está verificando se não há erros nos campos de e-mail e telefone
+            // (ou seja, emailError e phoneError são false). Se não houver erros,
+            // você atualiza os campos textResult e textResultPhone com os valores de email e phone.
+            if (!emailError && !phoneError) {
+                textResult.text = email
+                textResultPhone.text = phone
+            }
+        }
+        // Dentro do bloco de clique do botão "Limpar", você está redefinindo os campos de resultados
+        // textResult e textResultPhone) e os campos de entrada (textEmail e numPhone)
+        // para seus estados iniciais vazios ou nulos. Além disso, você está redefinindo as
+        // variáveis emailError e phoneError para false, para indicar que não há mais erros.
+        btLimpar.setOnClickListener {
+            textResultPhone.text = ""
+            textResult.text = ""
+            textEmail.text = null
+            numPhone.text = null
+            emailError = false
+            phoneError = false
+        }
 
 
 
@@ -49,4 +74,3 @@ class Projeto_2_Sozinho : AppCompatActivity() {
 
 
     }
-}
