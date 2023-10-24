1a861a1b09e6861111594be51410829068efba14
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-10-24 19:25:32 -0300

    código refeito apenas para melhor fixação de aprendizado.

--- app/src/main/java/com/raaisilvaa/appone/Projeto_2_Sozinho.kt
+++ app/src/main/java/com/raaisilvaa/appone/Projeto_2_Sozinho.kt
@@ -14,61 +14,48 @@ class Projeto_2_Sozinho : AppCompatActivity() {
 
         val textResult = findViewById<TextView>(R.id.textResult)
         val textResultPhone = findViewById<TextView>(R.id.textResultPhone)
-        val textEmail = findViewById<TextView>(R.id.textEmail)
+        val textEmail= findViewById<TextView>(R.id.textEmail)
         val numPhone = findViewById<TextView>(R.id.numPhone)
-        val btEnviar = findViewById<TextView>(R.id.btEnviar)
-        val btLimpar = findViewById<TextView>(R.id.btLimpar)
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val btLimpar = findViewById<Button>(R.id.btLimpar)
 
-        // Aqui, você está declarando duas variáveis booleanas, emailError e phoneError,
-        // para rastrear se há erros nos campos de e-mail e telefone.
-        // Inicialmente, eles são definidos como false, o que significa que não há erros.
-        var emailError = false
-        var phoneError = false
-
+        // Bot!ao clicável
         btEnviar.setOnClickListener {
-            val email = textEmail.text.toString()
-            val phone = numPhone.text.toString()
+            // Puxando váriaveis de fora da chave para dentro
+            val toEmail = textEmail.text.toString()
+            val toPhone = numPhone.text.toString()
 
-            // Você está limpando quaisquer erros anteriores, definindo as mensagens de erro para null,
-            // para garantir que as mensagens de erro anteriores não sejam exibidas.
-            textEmail.error = null
-            numPhone.error = null
-
-            if (email.isBlank() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            // Condições de validação de e-mail
+            if (toEmail.isBlank()) {
+                textEmail.error = "Digite seu e-mail!"
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(toEmail).matches()) {
                 textEmail.error = "E-mail inválido!"
-                emailError = true
             } else {
-                emailError = false
+                textResult.text = textEmail.text.toString()
             }
 
-            if (phone.isBlank()) {
-                numPhone.error = "Digite seu telefone"
-                phoneError = true
-            } else {
-                phoneError = false
+            // Função para validar o número de telefone para quantos números e caracteres, etc.
+            fun isValidPhoneNumber(phoneNumber: String): Boolean {
+                val pattern = Regex("^[0-9]{11,}\$")
+                return pattern.matches(phoneNumber)
             }
-            // Aqui, você está verificando se não há erros nos campos de e-mail e telefone
-            // (ou seja, emailError e phoneError são false). Se não houver erros,
-            // você atualiza os campos textResult e textResultPhone com os valores de email e phone.
-            if (!emailError && !phoneError) {
-                textResult.text = email
-                textResultPhone.text = phone
+            // Condições de validação de número de telefone puxando da função acima
+            if (toPhone.isBlank()) {
+                numPhone.error = "Digite seu número!"
+            } else if (!isValidPhoneNumber(toPhone)) {
+                numPhone.error = "Número inválido!"
+            } else {
+                textResultPhone.text = numPhone.text.toString()
             }
         }
-        // Dentro do bloco de clique do botão "Limpar", você está redefinindo os campos de resultados
-        // textResult e textResultPhone) e os campos de entrada (textEmail e numPhone)
-        // para seus estados iniciais vazios ou nulos. Além disso, você está redefinindo as
-        // variáveis emailError e phoneError para false, para indicar que não há mais erros.
+
+        // Botão clicável e com funcão de limpar todos os campos
         btLimpar.setOnClickListener {
-            textResultPhone.text = ""
             textResult.text = ""
+            textResultPhone.text = ""
             textEmail.text = null
             numPhone.text = null
-            emailError = false
-            phoneError = false
         }
-
-
 
 
 
