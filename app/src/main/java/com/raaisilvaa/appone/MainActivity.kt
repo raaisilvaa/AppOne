b9bb92fc412bca15fae4dd36e4312f742ff951db
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-10-25 23:03:39 -0300

    Implement Toast as feedback

--- app/src/main/java/com/raaisilvaa/appone/MainActivity.kt
+++ app/src/main/java/com/raaisilvaa/appone/MainActivity.kt
@@ -54,5 +54,6 @@ class MainActivity : AppCompatActivity() {
             digitePhone.text = null
         }
 
-}
+
+    }
 }
--- app/src/main/java/com/raaisilvaa/appone/Projeto_2_Sozinho.kt
+++ app/src/main/java/com/raaisilvaa/appone/Projeto_2_Sozinho.kt
@@ -3,9 +3,11 @@ package com.raaisilvaa.appone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import org.w3c.dom.Text
 
 class Projeto_2_Sozinho : AppCompatActivity() {
@@ -26,51 +28,54 @@ class Projeto_2_Sozinho : AppCompatActivity() {
             val toEmail = textEmail.text.toString()
             val toPhone = numPhone.text.toString()
 
+            fun isValidPhoneNumber(phoneNumber: String): Boolean {
+                val pattern = Regex("^[0-9]{11,}\$")
+                return pattern.matches(phoneNumber)
+            }
+
             // Condições de validação de e-mail
             if (toEmail.isBlank()) {
                 textEmail.error = "Digite seu e-mail!"
             } else if (!Patterns.EMAIL_ADDRESS.matcher(toEmail).matches()) {
                 textEmail.error = "E-mail inválido!"
-            } else {
-                textResult.text = textEmail.text.toString()
-            }
-
-            // Função para validar o número de telefone para quantos números e caracteres, etc.
-            fun isValidPhoneNumber(phoneNumber: String): Boolean {
-                val pattern = Regex("^[0-9]{11,}\$")
-                return pattern.matches(phoneNumber)
-            }
-            // Condições de validação de número de telefone puxando da função acima
-            if (toPhone.isBlank()) {
+            } else if (toPhone.isBlank()) {
                 numPhone.error = "Digite seu número!"
             } else if (!isValidPhoneNumber(toPhone)) {
                 numPhone.error = "Número inválido!"
             } else {
+                textResult.text = textEmail.text.toString()
                 textResultPhone.text = numPhone.text.toString()
+                // Mostrar o Toast somente quando ambos os campos forem válidos
+                Toast.makeText(this, "Atualizado com sucesso!", Toast.LENGTH_SHORT).show()
             }
+        }
 
-            // Animação na tela após certo comportamento
-            // THIS: ele referencia a Activy que está sendo trabalhada
-            // LENGTH SHORT: feedback mais curto
-            // LENGTH LONG: feedback mais longo
-
-            Toast.makeText(this, "Atualizado com sucesso!", Toast.LENGTH_SHORT).show() }
-
-       // Botão clicável e com funcão de limpar todos os campos
+        // Botão clicável e com funcão de limpar todos os campos
         btLimpar.setOnClickListener {
-            textResult.text = ""
-            textResultPhone.text = ""
+            val oldText = textResult.text
+            val oldPhone = textResultPhone.text
+
+            textResult.text = null
+            textResultPhone.text = null
             textEmail.text = null
             numPhone.text = null
+
+            val constraintLayout = findViewById<View>(R.id.constraintLayout)
+            Snackbar.make(constraintLayout, "Apagado com sucesso!", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Desfazer") {
+                    textResult.text = oldText
+                    textEmail.text = oldText
+                    textResultPhone.text = oldPhone
+                    numPhone.text = oldPhone
+
+
+                    Toast.makeText(this, "Desfeito com sucesso!", Toast.LENGTH_SHORT).show()
+                }
+
+                .show()
         }
 
-        // TODO: Implement Toast
-
-        // TODO: Implement Snackbar
-
-
 
         }
-
 
     }
